dbfb89ca48593f09036e2a0e9f89195cfa06d1d7
Shan <theo.pessegue2@gmail.com>
2024-02-09 20:45:31 +0100

    :art: Refactor InputManager class

--- src/models/inputsMangement/PlayerInput.ts
+++ src/models/inputsMangement/PlayerInput.ts
@@ -1,9 +1,8 @@
 import {
-	Scene,
-	ActionManager,
-	ExecuteCodeAction,
-	Scalar,
+    Scene,
+    Scalar,
 } from "@babylonjs/core";
+import { InputManager } from "./InputManager";
 
 // clavier français (azerty)
 const KEY_UP = "KeyW";
@@ -13,86 +12,67 @@ const KEY_RIGHT = "KeyD";
 const KEY_DASH = "KeyP";
 const KEY_JUMP = "Space";
 
-export class PlayerInput {
-	public inputMap: Record<string, boolean> = {};
-	public horizontal: number = 0;
-	public vertical: number = 0;
-	public horizontalAxis: number = 0;
-	public verticalAxis: number = 0;
-	public jumpKeyDown: boolean = false;
-	public dashing: boolean = false;
+export class PlayerInput extends InputManager {
+    public horizontal: number = 0;
+    public vertical: number = 0;
+    public horizontalAxis: number = 0;
+    public verticalAxis: number = 0;
+    public jumpKeyDown: boolean = false;
+    public dashing: boolean = false;
 
-	constructor(private scene: Scene) {
-		this.setupKeyboardInput();
-		this.setupBeforeRenderObservable();
+	constructor(scene: Scene) {
+		super(scene);
+		this.setupBeforeRenderObservable(this.updateFromKeyboard);
 	}
 
-	private setupKeyboardInput(): void {
-		this.scene.actionManager = new ActionManager(this.scene);
-		this.scene.actionManager.registerAction(
-			new ExecuteCodeAction(ActionManager.OnKeyDownTrigger, evt => {
-				this.inputMap[evt.sourceEvent.code] = true;
-				// console.log(evt.sourceEvent.code); // Affiche le code de la touche enfoncée dans la console
-			}),
-		);
-		this.scene.actionManager.registerAction(
-			new ExecuteCodeAction(ActionManager.OnKeyUpTrigger, evt => {
-				this.inputMap[evt.sourceEvent.code] = false;
-			}),
-		);
-	}
+	protected updateFromKeyboard = (): void => {
+        this.updateMovementInput();
+        this.updateJumpInput();
+        this.updateDashInput();
+    };
 
-	private setupBeforeRenderObservable(): void {
-		this.scene.onBeforeRenderObservable.add(this.updateFromKeyboard);
-	}
+    private updateMovementInput(): void {
+        const movementLerpSpeed = 0.2;
+        
+        // Forward/backward movement
+        if (this.inputMap[KEY_UP]) {
+            this.verticalAxis = 1;
+            this.vertical = Scalar.Lerp(this.vertical, 1, movementLerpSpeed);
+        } else if (this.inputMap[KEY_DOWN]) {
+            this.vertical = Scalar.Lerp(this.vertical, -1, movementLerpSpeed);
+            this.verticalAxis = -1;
+        } else {
+            this.vertical = 0;
+            this.verticalAxis = 0;
+        }
 
-	private updateFromKeyboard = (): void => {
-		this.updateMovementInput();
-		this.updateJumpInput();
-		this.updateDashInput();
-	};
+        // Left/right movement
+        if (this.inputMap[KEY_LEFT]) {
+            this.horizontal = Scalar.Lerp(this.horizontal, -1, movementLerpSpeed);
+            this.horizontalAxis = -1;
+        } else if (this.inputMap[KEY_RIGHT]) {
+            this.horizontal = Scalar.Lerp(this.horizontal, 1, movementLerpSpeed);
+            this.horizontalAxis = 1;
+        } else {
+            this.horizontal = 0;
+            this.horizontalAxis = 0;
+        }
+    }
 
-	private updateMovementInput(): void {
-		const movementLerpSpeed = 0.2;
-		
-		// Forward/backward movement
-		if (this.inputMap[KEY_UP]) {
-			this.verticalAxis = 1;
-			this.vertical = Scalar.Lerp(this.vertical, 1, movementLerpSpeed);
-		} else if (this.inputMap[KEY_DOWN]) {
-			this.vertical = Scalar.Lerp(this.vertical, -1, movementLerpSpeed);
-			this.verticalAxis = -1;
-		} else {
-			this.vertical = 0;
-			this.verticalAxis = 0;
-		}
+    private updateJumpInput(): void {
+        if (this.inputMap[KEY_JUMP]) {
+            this.jumpKeyDown = true;
+        } else {
+            this.jumpKeyDown = false;
+        }
+    }
 
-		// Left/right movement
-		if (this.inputMap[KEY_LEFT]) {
-			this.horizontal = Scalar.Lerp(this.horizontal, -1, movementLerpSpeed);
-			this.horizontalAxis = -1;
-		} else if (this.inputMap[KEY_RIGHT]) {
-			this.horizontal = Scalar.Lerp(this.horizontal, 1, movementLerpSpeed);
-			this.horizontalAxis = 1;
-		} else {
-			this.horizontal = 0;
-			this.horizontalAxis = 0;
-		}
-	}
-
-	private updateJumpInput(): void {
-		if (this.inputMap[KEY_JUMP]) {
-			this.jumpKeyDown = true;
-		} else {
-			this.jumpKeyDown = false;
-		}
-	}
-
-	private updateDashInput(): void {
-		if (this.inputMap[KEY_DASH]) {
-			this.dashing = true;
-		} else {
-			this.dashing = false;
-		}
-	}
+    private updateDashInput(): void {
+        if (this.inputMap[KEY_DASH]) {
+            this.dashing = true;
+        } else {
+            this.dashing = false;
+        }
+    }
 }
+
