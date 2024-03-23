f937e860a55dbbddfbaa36695d0198f0522f79d2
RemiSaurel <r.saurel5@gmail.com>
2024-03-23 11:34:41 +0100

    chore: add vue router

---
+++ src/components/landing/ClassicButton.vue
@@ -0,0 +1,19 @@
+<script setup lang="ts">
+const props = defineProps<{
+  text: string;
+}>();
+</script>
+
+<template>
+<button class="py-2 px-6 border-2 border-blue-darker bg-blue-lighter rounded-lg text-2xl font-bold w-64 specific-shadow">
+  <span class="shadow-white shadow-2xl">{{ props.text }}</span>
+</button>
+</template>
+
+<style scoped>
+.specific-shadow {
+  box-shadow:
+  -.1em .1em 0 #000,
+  -.2em .2em 0 #000;
+}
+</style>
