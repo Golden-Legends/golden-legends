bca84283e737f95270201d1647ee0f633c75668b
RemiSaurel <r.saurel5@gmail.com>
2024-03-23 16:55:48 +0100

    chore: add routes

--- src/router/routes.ts
+++ src/router/routes.ts
@@ -1,27 +1,33 @@
 import { createRouter, createWebHashHistory } from "vue-router";
 import LandingPage from "../pages/LandingPage.vue";
 import BabylonPage from "../pages/BabylonPage.vue";
+import LoginPage from "../pages/LoginPage.vue";
 
 const routes = [
-    {
-        path: "/",
-        name: "Landing",
-        component: LandingPage,
-    },
-    {
-        path: "/game",
-        name: "Game",
-        component: BabylonPage,
-    },
-    {
-        path: "/:catchAll(.*)",
-        redirect: "/",
-    },
+  {
+    path: "/",
+    name: "Landing",
+    component: LandingPage,
+  },
+  {
+    path: "/game",
+    name: "Game",
+    component: BabylonPage,
+  },
+  {
+    path: "/login",
+    name: "Login",
+    component: LoginPage,
+  },
+  {
+    path: "/:catchAll(.*)",
+    redirect: "/",
+  },
 ];
 
 const router = createRouter({
-    history: createWebHashHistory("/"),
-    routes: routes,
+  history: createWebHashHistory("/"),
+  routes: routes,
 });
 
 export default router;
