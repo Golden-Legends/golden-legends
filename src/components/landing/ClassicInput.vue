33acea553e257691283f3b8bd8415a94dccb0de9
RemiSaurel <r.saurel5@gmail.com>
2024-03-23 11:36:20 +0100

    feat: input button

---
+++ src/components/landing/ClassicInput.vue
@@ -0,0 +1,12 @@
+<script setup lang="ts">
+const props = defineProps<{
+  placeholder: string;
+}>();
+</script>
+
+<template>
+  <input type="text" class="py-2 px-6 border-2 border-blue-darker bg-blue-lighter rounded-lg text-lg font-bold w-64 specific-shadow" :placeholder="props.placeholder" />
+</template>
+
+<style scoped>
+</style>
