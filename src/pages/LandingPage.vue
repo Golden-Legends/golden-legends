3689abae44f0c28b6e09ff8286dba7bcd2576035
RemiSaurel <r.saurel5@gmail.com>
2024-03-23 16:55:58 +0100

    feat: landing and login pages

--- src/pages/LandingPage.vue
+++ src/pages/LandingPage.vue
@@ -1,5 +1,5 @@
 <script setup lang="ts">
-import {useRouter} from "vue-router";
+import { useRouter } from "vue-router";
 import ClassicButton from "@/components/landing/ClassicButton.vue";
 import ClassicInput from "@/components/landing/ClassicInput.vue";
 
@@ -7,28 +7,27 @@ const router = useRouter();
 </script>
 
 <template>
-<div class="w-screen h-screen flex">
-  <div class="w-1/2">
-    <div class="flex flex-col items-center justify-center mt-24">
-      <img src="@/assets/landing_logo.svg" alt="Vue logo" />
-      <div class="flex flex-col items-center gap-4 mt-16">
-        <ClassicButton text="SE CONNECTER" @click="router.push('/game')" />
-        <span class="text-2xl font-bold">OU</span>
-        <ClassicInput placeholder="Ex : XxGamerdu12xX" />
-        <ClassicButton text="JOUER" @click="router.push('/game')" />
+  <div class="w-screen h-screen flex">
+    <div class="w-full lg:w-1/2">
+      <div class="flex flex-col items-center justify-center mt-24">
+        <img src="@/assets/landing_logo.svg" alt="Vue logo" />
+        <div class="flex flex-col items-center gap-4 mt-16">
+          <ClassicButton
+            text="SE CONNECTER"
+            @click="router.push({ name: 'Login' })"
+          />
+          <span class="text-2xl font-bold">OU</span>
+          <ClassicInput placeholder="Ex : XxGamerdu12xX" />
+          <ClassicButton text="JOUER" @click="router.push({ name: 'Game' })" />
+        </div>
       </div>
     </div>
-
-  </div>
-  <div class="w-1/2 bg-white ">
-    <div class="absolute -right-24 top-32">
-      <img src="@/assets/landing.svg" alt="Vue logo" />
-
+    <div class="hidden lg:block lg:w-1/2 bg-white">
+      <div class="absolute -right-24 top-32">
+        <img src="@/assets/landing.svg" alt="Vue logo" />
+      </div>
     </div>
   </div>
-</div>
 </template>
 
-<style scoped>
-
-</style>
+<style scoped></style>
--- src/pages/LoginPage.vue
+++ src/pages/LoginPage.vue
@@ -1,11 +1,119 @@
 <script setup lang="ts">
+import { ref } from "vue";
+import ConnexionButton from "@/components/connexion/ConnexionButton.vue";
+import { UserService } from "@/services/user-service.ts";
+import { useRouter } from "vue-router";
 
+const router = useRouter();
+const toggle = ref("login");
+
+const email = ref("");
+const password = ref("");
+const username = ref("");
+
+const login = async () => {
+  try {
+    await UserService.login(username.value, password.value);
+    await router.push({ name: "Game" });
+  } catch (error) {}
+};
+
+const register = async () => {
+  try {
+    await UserService.register(username.value, email.value, password.value);
+    await router.push({ name: "Game" });
+  } catch (error) {}
+};
 </script>
 
 <template>
-  $END$
+  <div class="w-screen h-screen flex flex-col items-center mt-20">
+    <div
+      class="flex items-center gap-1 border bg-white h-fit w-fit p-2 rounded-lg"
+    >
+      <button
+        class="p-3 rounded transition-all duration-300"
+        :class="toggle === 'login' ? 'bg-blue-lighter/50' : ''"
+        @click="toggle = 'login'"
+      >
+        Connexion
+      </button>
+      <button
+        class="p-3 rounded transition-all duration-300"
+        :class="toggle === 'signup' ? 'bg-blue-lighter/50' : ''"
+        @click="toggle = 'signup'"
+      >
+        Inscription
+      </button>
+    </div>
+
+    <div
+      v-if="toggle === 'login'"
+      class="flex flex-col gap-4 mt-4 items-center"
+    >
+      <div class="flex flex-col">
+        <label class="text-lg font-bold text-blue-darker"
+          >Nom d'utilisateur</label
+        >
+        <input
+          type="text"
+          class="py-2 px-4 border-2 border-blue-darker bg-blue-lighter rounded-lg font-bold w-72"
+          placeholder="XxGamerdu12xX"
+          v-model="username"
+        />
+      </div>
+
+      <div class="flex flex-col">
+        <label class="text-lg font-bold text-blue-darker">Mot de passe</label>
+        <input
+          type="password"
+          class="py-2 px-4 border-2 border-blue-darker bg-blue-lighter rounded-lg font-bold w-72"
+          placeholder="lebgdu12"
+          @keyup.enter="login"
+          v-model="password"
+        />
+      </div>
+      <ConnexionButton @click="login()" text="Se connecter" />
+    </div>
+    <div
+      v-if="toggle === 'signup'"
+      class="flex flex-col gap-4 mt-4 items-center"
+    >
+      <div class="flex flex-col">
+        <label class="text-lg font-bold text-blue-darker"
+          >Nom d'utilisateur</label
+        >
+        <input
+          type="text"
+          class="py-2 px-4 border-2 border-blue-darker bg-blue-lighter rounded-lg font-bold w-72"
+          placeholder="XxGamerdu12xX"
+          v-model="username"
+        />
+      </div>
+
+      <div class="flex flex-col">
+        <label class="text-lg font-bold text-blue-darker">Email</label>
+        <input
+          type="email"
+          class="py-2 px-4 border-2 border-blue-darker bg-blue-lighter rounded-lg font-bold w-72"
+          placeholder="lebgdu12@gmail.com"
+          v-model="email"
+        />
+      </div>
+
+      <div class="flex flex-col">
+        <label class="text-lg font-bold text-blue-darker">Mot de passe</label>
+        <input
+          type="password"
+          class="py-2 px-4 border-2 border-blue-darker bg-blue-lighter rounded-lg font-bold w-72"
+          placeholder="lebgdu12"
+          v-model="password"
+          @keyup.enter="register"
+        />
+      </div>
+      <ConnexionButton @click="register" text="S'inscrire" />
+    </div>
+  </div>
 </template>
 
-<style scoped>
-
-</style>
+<style scoped></style>
