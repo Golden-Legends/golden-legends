0e191c6934c9f061e95bca9e5aca026a7f61cb75
RemiSaurel <r.saurel5@gmail.com>
2024-04-03 19:35:02 +0200

    style: add custom colors

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,17 +1,15 @@
 /** @type {import('tailwindcss').Config} */
 export default {
-  content: [
-    './src/**/*.html',
-    './src/**/*.vue',
-  ],
+  content: ["./src/**/*.html", "./src/**/*.vue"],
   theme: {
     extend: {
       colors: {
-        "blue-darker": '#005988',
-        "blue-lighter": '#C8ECFF',
+        "blue-darker": "#005988",
+        "blue-lighter": "#C8ECFF",
+        "begin-gradient": "#0092DF",
+        "end-gradient": "#00466B",
       },
     },
   },
   plugins: [],
-}
-
+};
