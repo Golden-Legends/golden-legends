7a9eb766935a0b786f6ff768f1d569a97c067b1f
RemiSaurel <r.saurel5@gmail.com>
2024-04-03 19:35:31 +0200

    feat: sandbox components

---
+++ src/components/SandboxContainer.vue
@@ -0,0 +1,16 @@
+<script setup lang="ts">
+const props = defineProps({
+  name: String,
+});
+</script>
+
+<template>
+  <div class="flex flex-col gap-2">
+    <span class="text-xl">{{ props.name }}</span>
+    <div class="flex gap-2 overflow-auto p-2">
+      <slot />
+    </div>
+  </div>
+</template>
+
+<style scoped></style>
