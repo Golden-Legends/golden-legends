5638a264df8939b9380bd2d6229a448b2503b531
RemiSaurel <r.saurel5@gmail.com>
2024-04-04 17:14:25 +0200

    chore: assets

---
+++ src/components/gui/Dialog.vue
@@ -0,0 +1,38 @@
+<script setup lang="ts">
+import { onMounted, ref } from "vue";
+
+const timer = ref(0);
+
+const timerToSMS = (time: number): string => {
+  const seconds = Math.floor(time / 100);
+  const milliseconds = time % 100;
+  return `${seconds}.${milliseconds < 10 ? "0" : ""}${milliseconds}`;
+};
+
+const startTimer = () => {
+  setInterval(() => {
+    if (timer.value < 1000) {
+      timer.value += 1;
+    } else {
+      clearTimeout(this);
+    }
+  }, 10);
+};
+
+onMounted(() => {
+  startTimer();
+});
+</script>
+
+<template>
+  <div
+    class="flex justify-between border-4 border-black bg-gradient-to-b from-begin-blue-gradient to-end-blue-gradient text-white w-64 items-center p-2 px-4 rounded-lg"
+  >
+    <img src="@/assets/timer_logo.svg" alt="timer logo" class="mr-16" />
+    <div class="font-bold text-2xl">
+      {{ timerToSMS(timer) }}
+    </div>
+  </div>
+</template>
+
+<style scoped></style>
---
+++ src/components/gui/options/Options.vue
@@ -0,0 +1,33 @@
+<script setup lang="ts">
+import ClassicButton from "@/components/landing/ClassicButton.vue";
+</script>
+
+<template>
+  <div
+    class="relative border-4 border-black bg-gradient-to-b from-begin-blue-gradient to-end-blue-gradient text-white items-center p-2 px-4 rounded-lg h-fit w-[400px] h-min-[100px]"
+  >
+    <div
+      class="absolute -right-6 text-4xl font-bold w-12 h-12 rounded-lg border-4 border-black bg-gradient-to-b from-begin-red-gradient to-end-red-gradient text-white"
+    >
+      <button
+        class="flex items-center justify-center h-full w-full p-2 transition-all hover:bg-red-800"
+      >
+        <img class="w-full h-full" src="@/assets/close.svg" alt="close" />
+      </button>
+    </div>
+
+    <div class="w-full mt-1">
+      <div class="uppercase text-4xl font-black text-stroke-2">Options</div>
+    </div>
+
+    <div class="flex flex-col gap-4 my-4">
+      <slot />
+    </div>
+
+    <div class="flex flex-col gap-4 my-4">
+      <ClassicButton text="Menu Principal" />
+    </div>
+  </div>
+</template>
+
+<style scoped></style>
