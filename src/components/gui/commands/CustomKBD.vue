5dd6d890f80568e624bee7c461ee065b5901bf38
RemiSaurel <r.saurel5@gmail.com>
2024-04-09 20:02:49 +0200

    feat: custom command composent

--- src/components/gui/commands/CustomKBD.vue
+++ src/components/gui/commands/CustomKBD.vue
@@ -1,11 +1,15 @@
 <script setup lang="ts">
-
+const props = defineProps({
+  keybind: String,
+});
 </script>
 
 <template>
-  $END$
+  <kbd
+    class="border-2 border-black bg-gray-400 text-white text-2xl font-black p-1 px-2 rounded-lg h-12 w-fit min-w-[3rem] flex items-center justify-center font-sans"
+  >
+    <span class="font-black text-2xl uppercase">{{ props.keybind }}</span>
+  </kbd>
 </template>
 
-<style scoped>
-
-</style>
+<style scoped></style>
