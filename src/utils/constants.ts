7f3b83768b21f1428ed8e9a0665e8f25b2d5155f
RemiSaurel <r.saurel5@gmail.com>
2024-04-13 14:42:06 +0200

    feat: characters name / path

--- src/utils/constants.ts
+++ src/utils/constants.ts
@@ -1,1 +1,24 @@
 export const BACK_URL = import.meta.env.VITE_BACKEND;
+
+export const CHARACTERS = [
+  {
+    name: "Seb",
+    path: "1",
+  },
+  {
+    name: "Théo",
+    path: "2",
+  },
+  {
+    name: "Nico",
+    path: "3",
+  },
+  {
+    name: "Rémi",
+    path: "4",
+  },
+  {
+    name: "Cédric",
+    path: "5",
+  },
+];
