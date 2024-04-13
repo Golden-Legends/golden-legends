1f7257b4c524d129758faf9f6cc549029b6b91fc
RemiSaurel <r.saurel5@gmail.com>
2024-04-13 14:42:32 +0200

    feat: characters selection components

--- src/components/characters/PlayButton.vue
+++ src/components/characters/PlayButton.vue
@@ -1,11 +1,19 @@
-<script setup lang="ts">
-
-</script>
+<script setup lang="ts"></script>
 
 <template>
-  $END$
+  <button
+    class="group py-2 px-6 border-2 border-blue-darker bg-blue-lighter rounded-lg text-2xl font-bold w-64 specific-shadow hover:bg-blue-darker hover:border-blue-lighter transition-all duration-300"
+  >
+    <span class="group-hover:text-white transition-all duration-300"
+      >Jouer</span
+    >
+  </button>
 </template>
 
 <style scoped>
-
+.specific-shadow {
+  box-shadow:
+    -0.1em 0.1em 0 #000,
+    -0.2em 0.2em 0 #000;
+}
 </style>
