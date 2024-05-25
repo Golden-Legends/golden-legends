af21fec67f55691d946f5597eff27897d380c8b7
RemiSaurel <r.saurel5@gmail.com>
2024-05-25 10:10:45 +0200

    feat: disabled characters
    Depending on mini-game states

--- src/components/characters/Character.vue
+++ src/components/characters/Character.vue
@@ -4,6 +4,7 @@ const props = defineProps<{
     name: string;
     path: string;
   };
+  enabled: boolean;
 }>();
 
 const getAsset = (name: string) => {
@@ -13,11 +14,12 @@ const getAsset = (name: string) => {
 
 <template>
   <button
-    class="w-40 h-40 rounded-2xl bg-blue-lighter flex items-center justify-center transition duration-500 group hover:rotate-12 specific-shadow"
+    class="w-40 h-40 rounded-2xl bg-blue-lighter flex items-center justify-center transition duration-500 group enabled:hover:rotate-12 specific-shadow disabled:opacity-30 disabled:cursor-not-allowed"
     @click="$emit('select-character', props.character.path)"
+    :disabled="!props.enabled"
   >
     <img
-      class="w-full h-full rounded-full scale-125 group-hover:scale-150 transition duration-500 pointer-events-none"
+      class="w-full h-full rounded-full scale-125 enabled:group-hover:scale-150 transition duration-500 pointer-events-none"
       :src="getAsset(props.character.path)"
       alt="character"
     />
