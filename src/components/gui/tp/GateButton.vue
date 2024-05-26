0bf35a3368540188a10917733f7f92736ca7c7c3
RemiSaurel <r.saurel5@gmail.com>
2024-05-26 19:36:39 +0200

    fix: tp local storage

--- src/components/gui/tp/GateButton.vue
+++ src/components/gui/tp/GateButton.vue
@@ -20,12 +20,10 @@ const difficulty = ref(localStorage.getItem(props.game));
 
 const disabled = computed(() => {
   if (!difficulty.value) {
-    // Wait 2sec and try again until localStorage is ready
-    while (!difficulty.value) {
-      setTimeout(() => {
-        difficulty.value = localStorage.getItem(props.game);
-      }, 2000);
-    }
+    // Wait 10sec and try again until localStorage is ready
+    setTimeout(() => {
+      difficulty.value = localStorage.getItem(props.game);
+    }, 10000);
     return true;
   } else if (difficulty.value === props.difficulty) {
     return false;
