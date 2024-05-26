e15c0e37457019610d89d1e33b79dee3e47ce056
Shan <theo.pessegue2@gmail.com>
2024-05-26 09:33:40 +0200

    add public with instance for natation game

--- src/models/environments/natatonGameEnv.ts
+++ src/models/environments/natatonGameEnv.ts
@@ -1,14 +1,18 @@
 import { Scaling } from "@/utils/Scaling";
-import { Matrix, Mesh, MeshBuilder, Scene, SceneLoader, Vector3 } from "@babylonjs/core";
+import { AssetContainer, Matrix, Mesh, MeshBuilder, Scene, SceneLoader, Vector3 } from "@babylonjs/core";
 import { SkyMaterial } from "@babylonjs/materials";
+import { InstanceManager } from "./InstManager";
 
 export class natationGameEnv {
     private _scene: Scene;
 	public assets;
 	public filename: string[] = ["perso1.glb", "perso2.glb", "perso3.glb", "perso4.glb", "perso5.glb", "perso6.glb", "perso7.glb"];
-
+	public assetContainerTab: AssetContainer[] = [];
+	public parentMesh: Mesh;
+	
     constructor(scene: Scene) {
 		this._scene = scene;
+		this.parentMesh = new Mesh("parentPublic", this._scene);
 	}
 
     public async load() {
@@ -19,9 +23,64 @@ export class natationGameEnv {
 			m.checkCollisions = true;
 		});
 		this.createSkybox(this._scene);
-		this.loadPublic();
+
+		const outer = MeshBuilder.CreateBox(
+			"masterOuter",
+			{ width: 1, depth: 1, height: 15 },
+			this._scene,
+		);
+		// pour afficher la box qui sert de collision
+		outer.isVisible = false;
+		outer.isPickable = false;
+		outer.checkCollisions = true;
+		//move origin of box collider to the bottom of the mesh (to match player mesh)
+		outer.bakeTransformIntoVertices(Matrix.Translation(0, 7, 0));
+		//for collisions
+		outer.ellipsoid = new Vector3(1, 1.5, 1);
+		outer.ellipsoidOffset = new Vector3(0, 1.5, 0);
+		this.parentMesh = outer;
+
+		this.assetContainerTab = await InstanceManager.initInstance(this.filename, this._scene, "./models/characters/");
+		await this.loadPublic();
 	}
 
+	
+	private loadInstance(i : number, position : Vector3, rotation : Vector3) {
+		// position, i, "public" + i, rotation
+		const child = InstanceManager.duplicateParentMesh(this.parentMesh, `publicNatation${i}`);
+		const res = InstanceManager.duplicateInstance(this.assetContainerTab[i], position, rotation, child, 0.018);
+		const idle = res.animationGroups.find(ag => ag.name.includes("idle"));
+		const applause = res.animationGroups.find(ag => ag.name.includes("applause"));
+
+		let randomNumber = Math.floor(Math.random() * 2) + 1;
+		if(randomNumber == 1){
+			if(applause){
+				idle?.stop();
+				applause.play(true);
+			}
+		} else {
+			if(idle){
+				idle.stop();
+				idle.play(true);
+			}
+		}
+		setTimeout(() => {
+			if(randomNumber == 1){
+				if(applause){
+					idle?.stop();
+					applause.stop();
+					applause.play(true);
+				}
+			} else {
+				if(idle){
+					idle.stop();
+					applause?.stop();
+					idle.play(true);
+				}
+			}
+		}, randomNumber);
+	}
+	
 	public async loadPublic() {
 		let randomNumber1 = Math.floor(Math.random() * 6) + 1;
 		let compteur = 0;
@@ -58,7 +117,7 @@ export class natationGameEnv {
 						rotation = new Vector3(0, 80, 0);
 					}
 					let position = new Vector3(-pos.position.x, pos.position.y, pos.position.z);
-					await this._loadCharacterAssets(this._scene, position, this.filename[i-tour*6], "public" + i, rotation);
+					this.loadInstance(i-tour*6, position, rotation)
 				}
 				compteur += 1;	
 				if(compteur === 6){
@@ -98,7 +157,8 @@ export class natationGameEnv {
 						rotation = new Vector3(0, 40, 0);
 					}
 					let position = new Vector3(-pos.position.x, pos.position.y, pos.position.z);
-					await this._loadCharacterAssets(this._scene, position, this.filename[i-tour*6], "public" + i, rotation);
+					// await this._loadCharacterAssets(this._scene, position, this.filename[i-tour*6], "public" + i, rotation);
+					this.loadInstance(i-tour*6, position, rotation)
 				}
 				compteur += 1;	
 				if(compteur === 6){
@@ -119,7 +179,8 @@ export class natationGameEnv {
 			let pos = this._scene.getMeshByName("perso1." + i);
 			if(pos){
 				let position = new Vector3(-pos.position.x, pos.position.y, pos.position.z);
-				await this._loadCharacterAssets(this._scene, position, this.filename[Math.floor(Math.random() * 6) + 1], "public" + i, rotation);
+				// await this._loadCharacterAssets(this._scene, position, this.filename[Math.floor(Math.random() * 6) + 1], "public" + i, rotation);
+				this.loadInstance(i-tour*6, position, rotation)
 			}
 		}
 
@@ -152,77 +213,5 @@ export class natationGameEnv {
 		skyMaterial.inclination = 0;
 		const skyBox = MeshBuilder.CreateBox("skyBox", { size: 2500.0 }, scene);
 		skyBox.material = skyMaterial;
-	}
-
-	private async _loadCharacterAssets(scene: Scene, position: Vector3, path: string, name: string, rotation: Vector3){
-
-		async function loadCharacter(){
-			//collision mesh
-			const outer = MeshBuilder.CreateBox(
-				name,
-				{ width: 1, depth: 1, height: 15 },
-				scene,
-			);
-			// pour afficher la box qui sert de collision
-			outer.isVisible = false;
-			outer.isPickable = false;
-			outer.checkCollisions = true;
-			//move origin of box collider to the bottom of the mesh (to match player mesh)
-			outer.bakeTransformIntoVertices(Matrix.Translation(0, 7, 0));
-			//for collisions
-			outer.ellipsoid = new Vector3(1, 1.5, 1);
-			outer.ellipsoidOffset = new Vector3(0, 1.5, 0);
-
-			//--IMPORTING MESH--
-			return SceneLoader.ImportMeshAsync(
-				null,
-				"./models/characters/",
-				path,
-				scene,
-			).then(result => {
-				const root = result.meshes[0];
-				//body is our actual player mesh
-				const body = root;
-				body.parent = outer;
-				body.isPickable = false;
-				body.getChildMeshes().forEach(m => {
-					m.isPickable = false;
-				});
-				body.scaling = new Scaling(0.018);
-				body.showBoundingBox = true;
-
-                outer.position = position;
-                outer.rotation = rotation;
-
-				// enlever l'animation à l'indice 0 de animationsGroups
-				result.animationGroups[0].stop();
-				
-				let randomNumber = Math.floor(Math.random() * 2) + 1;
-				if(randomNumber == 1){
-					const idle = result.animationGroups.find(ag => ag.name === "Anim|idle");
-					if(idle){
-						idle.loopAnimation = true;
-						idle.play(true);
-					}
-				}
-				else{
-					const applause = result.animationGroups.find(ag => ag.name === "Anim|applause");
-					if(applause){
-						applause.loopAnimation = true;
-						applause.play(true);
-					}
-				}
-				//return the mesh and animations
-				return {
-					mesh: outer as Mesh,
-					animationGroups: result.animationGroups,
-				};
-			});
-		}	
-
-		return loadCharacter().then(assets=> {
-			this.assets = assets;
-		})
-
 	}
 }
