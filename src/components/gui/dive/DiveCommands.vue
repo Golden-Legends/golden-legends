dd02b808bf597748786a7e4c6d9b91f527b85a8d
RemiSaurel <r.saurel5@gmail.com>
2024-05-27 09:47:10 +0200

    fix: placement

--- src/components/gui/dive/DiveCommands.vue
+++ src/components/gui/dive/DiveCommands.vue
@@ -16,25 +16,29 @@ const props = defineProps({
 </script>
 
 <template>
-  <div
-    v-for="(keybind, index) in props.keybinds"
-    class="border-4 mt-4 border-black rounded-lg h-12 w-fit min-w-[3rem]"
-    :class="{
-      'bg-green-700': props.resultState[index],
-      'bg-red-700': props.resultState[index] === false,
-      'border-black animate-bounce	': index === props.index,
-      'border-neutral-300': index !== props.index,
-    }"
-  >
-    <div
-      class="w-full h-full rounded p-1 px-2 flex items-center justify-center"
-    >
-      <span
-        v-if="index < props.index"
-        class="font-black font-sans text-2xl uppercase text-white"
+  <div class="relative">
+    <div class="flex gap-2 justify-center">
+      <div
+        v-for="(keybind, index) in props.keybinds"
+        class="border-4 mt-4 border-black rounded-lg h-12 w-fit min-w-[3rem]"
+        :class="{
+          'bg-green-700': props.resultState[index],
+          'bg-red-700': props.resultState[index] === false,
+          'border-black animate-bounce	': index === props.index,
+          'border-neutral-300': index !== props.index,
+        }"
       >
-        {{ keybind }}
-      </span>
+        <div
+          class="w-full h-full rounded p-1 px-2 flex items-center justify-center"
+        >
+          <span
+            v-if="index < props.index"
+            class="font-black font-sans text-2xl uppercase text-white"
+          >
+            {{ keybind }}
+          </span>
+        </div>
+      </div>
     </div>
   </div>
 </template>
