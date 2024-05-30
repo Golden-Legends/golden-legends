0a4a86b1501faaee24365225f0746545a2cb42ff
Shan <theo.pessegue2@gmail.com>
2024-05-30 19:03:41 +0200

    init with a new vector for movement

--- src/models/scene/games/TennisGameState.ts
+++ src/models/scene/games/TennisGameState.ts
@@ -2,7 +2,7 @@ import { Game } from "@/models/Game";
 import { GameState } from "@/models/GameState";
 import { TennisGameEnv } from "@/models/environments/tennisGameEnv";
 import { PlayerInputTennisGame } from "@/models/inputsMangement/PlayerInputTennisGame";
-import { Color3, HemisphericLight, Mesh, MeshBuilder, Scene, StandardMaterial, UniversalCamera, Vector3 } from "@babylonjs/core";
+import { AbstractMesh, Color3, HemisphericLight, Mesh, MeshBuilder, Scene, StandardMaterial, UniversalCamera, Vector3 } from "@babylonjs/core";
 
 export class TennisGameState extends GameState{
 		private _camera: UniversalCamera;
@@ -10,19 +10,16 @@ export class TennisGameState extends GameState{
 		
 		private isMultiplayer: boolean = false;
 		// JOUEURS
-		private joueur1 !: Mesh;
-		private joueur2 !: Mesh;
+		
 		private input : PlayerInputTennisGame;
 		// BALLE 
-		private ball : Ball;
+		private ball !: Ball;
+		_paddle1!: Paddle;
+    _paddle2!: Paddle;
 		private ballSpeed = 0.05;
 		// DEPLACEMENT
 		private speed = 0.05;
-		// LIMITATION
-		private minX = 0;
-		private maxX = 0;
-		private minZ = 0;
-		private maxZ = 0;
+		
 
 		private score = {
 			player1: 0,
@@ -41,7 +38,7 @@ export class TennisGameState extends GameState{
 				this._camera = this.createCamera();
 				this.input = new PlayerInputTennisGame(this.scene);
 				this.environment = new TennisGameEnv(this.scene);
-				this.ball = new Ball(this.scene);
+				
     }
 
 		// cameraJoueur, joueur1, filet, joueur2, boite
@@ -52,7 +49,6 @@ export class TennisGameState extends GameState{
 				this.scene.detachControl();
 
 				await this.setEnvironment();
-				this.ball.initBall();
 				this.createLight();
 
 				const boite = this.scene.getMeshByName("boite");
@@ -66,14 +62,19 @@ export class TennisGameState extends GameState{
 					this._camera.position = new Vector3(-cameraMesh.position.x, cameraMesh.position.y + 1.5, cameraMesh.position.z - 0.7);
 				}
 
+				//add paddles to the scene
+        this._paddle1 = new Paddle('player', this.scene, this.input);
+        this._paddle2 = new Paddle('cpu', this.scene, this.input);
+				this.ball = new Ball(this.scene, this._paddle1, this._paddle2);
+
 				if (this.isMultiplayer) {
 					console.log("multiplayer");
 				} else {
 					await this.initPlayer();
 				}
 
-				this.joueur1.checkCollisions = true;
-				console.log("player : ",  this.joueur1.checkCollisions)
+				// this.joueur1.checkCollisions = true;
+				// console.log("player : ",  this.joueur1.checkCollisions)
 				this.runUpdateAndRender();
 				await this.scene.whenReadyAsync(); // on attends que la scene soit bien chargé
 				this.scene.attachControl();
@@ -92,7 +93,10 @@ export class TennisGameState extends GameState{
 
     update(): void {
 			try {
-				this.deplacementJoueur1();
+				// this.deplacementJoueur1();
+				// this.ball.update();
+				this._paddle1.handleEvent();
+				this._paddle2.moveByBallPosition(this.ball._body.position.x);
 				this.ball.update();
 			} catch (error) {
         throw new Error("Method not implemented.");
@@ -101,32 +105,7 @@ export class TennisGameState extends GameState{
 
     async setEnvironment(): Promise<void> {
 			try { 
-				await this.environment.load();
-
-				const coteG = this.scene.getMeshByName("coteG");
-				if (coteG) {
-					coteG.isVisible = false;
-					this.maxX = coteG.getAbsolutePosition().x + -0.2;
-				}
-
-				const coteD = this.scene.getMeshByName("coteR");
-				if (coteD) {
-					coteD.isVisible = false;
-					this.minX = coteD.getAbsolutePosition().x + 0.2;
-				}
-				const finJ1 = this.scene.getMeshByName("finJ1");
-				if (finJ1) {
-					finJ1.isVisible = false;
-					this.minZ = finJ1.getAbsolutePosition().z;
-				}
-				const finJ2 = this.scene.getMeshByName("finJ2");
-				if (finJ2) {
-					finJ2.isVisible = false;
-					this.maxZ = finJ2.getAbsolutePosition().z;
-				}
-
-				
-
+				await this.environment.load();	
 			} catch (error) {
 				throw new Error("Method not implemented.");
 			}
@@ -148,75 +127,301 @@ export class TennisGameState extends GameState{
 		}
 
 		private async initPlayer() {
-			const player1MeshBlender = this.scene.getMeshByName("joueur1");
-			this.joueur1 = MeshBuilder.CreateBox("joueur1", {width: 0.4, height: 0.3, depth: 0.1}, this.scene);
-			this.joueur1.position = player1MeshBlender?.getAbsolutePosition() as Vector3;
-			// mettre une couleur rouge 
-			const material = new StandardMaterial("texture1", this.scene);
-			material.emissiveColor = new Color3(0, 0, 1);
-			this.joueur1.material = material;	
-			// J2
-			const player2MeshBlender = this.scene.getMeshByName("joueur2");
-			this.joueur2 = MeshBuilder.CreateBox("joueur2", {width: 0.4, height: 0.3, depth: 0.1}, this.scene);
-			this.joueur2.position = player2MeshBlender?.getAbsolutePosition() as Vector3;
-			// mettre une couleur bleu
-			const material2 = new StandardMaterial("texture2", this.scene);
-			material2.emissiveColor = new Color3(1, 0, 0);
-			this.joueur2.material = material2;
+			// const player1MeshBlender = this.scene.getMeshByName("joueur1");
+			// this.joueur1 = MeshBuilder.CreateBox("joueur1", {width: 0.4, height: 0.3, depth: 0.1}, this.scene);
+			// this.joueur1.position = player1MeshBlender?.getAbsolutePosition() as Vector3;
+			// // mettre une couleur rouge 
+			// const material = new StandardMaterial("texture1", this.scene);
+			// material.emissiveColor = new Color3(0, 0, 1);
+			// this.joueur1.material = material;	
+			// // J2
+			// const player2MeshBlender = this.scene.getMeshByName("joueur2");
+			// this.joueur2 = MeshBuilder.CreateBox("joueur2", {width: 0.4, height: 0.3, depth: 0.1}, this.scene);
+			// this.joueur2.position = player2MeshBlender?.getAbsolutePosition() as Vector3;
+			// // mettre une couleur bleu
+			// const material2 = new StandardMaterial("texture2", this.scene);
+			// material2.emissiveColor = new Color3(1, 0, 0);
+			// this.joueur2.material = material2;
 		}	
 
 		private deplacementJoueur1() {
-			const newX = this.joueur1.position.x + this.input.horizontal * this.speed;
-			// Vérification des limites
-			if (newX < this.minX) {
-				this.joueur1.position.x = this.minX;
-			} else if (newX > this.maxX) {
-				this.joueur1.position.x = this.maxX;
-			} else {
-				this.joueur1.position.x = newX;
-			}
+			// const newX = this.joueur1.position.x + this.input.horizontal * this.speed;
+			// // Vérification des limites
+			// if (newX < this.minX) {
+			// 	this.joueur1.position.x = this.minX;
+			// } else if (newX > this.maxX) {
+			// 	this.joueur1.position.x = this.maxX;
+			// } else {
+			// 	this.joueur1.position.x = newX;
+			// }
 		}
 }
 
 class Ball {
-	private direction: Vector3;
-	private speed: number;
-	private ball: Mesh;
-	private scene: Scene;
-	private hasStarted = false;
+	_body: Mesh;
+	_speed: number;
+	_direction: BallDirection;
+	_paddle1: Paddle;
+	_paddle2: Paddle;
+	meshFinJ1: AbstractMesh;
+	meshFinJ2: AbstractMesh;
+	private _score = {
+		player1: 0,
+		player2: 0
+	}
+	private _isEndGame = false;
+	private indexGame = 0;
+	private maxGame = 3;
+	private resetBall = false;
+	private resetPlacement : Vector3 = new Vector3(0, 0, 0);
+	_velocity: Vector3;
+	// LIMITATION
+	private minX = 0;
+	private maxX = 0;
+	private minZ = 0;
+	private maxZ = 0;
+	private _scene : Scene;
+	
+	constructor(scene: Scene, paddle1, paddle2: Paddle) {
+			// create a ball
+			this._scene = scene;
+			this._body = MeshBuilder.CreateSphere("ball", {
+					diameter: 0.1,
+					updatable: true
+			}, scene);
 
-	constructor(scene: Scene) {
-		this.scene = scene;
-		this.speed = 0.01;
-		this.ball = new Mesh("ball");
-		this.direction = new Vector3(0, 0, 0);
+			// prepare the material
+			let mat = new StandardMaterial('ballMaterial', scene);
+			mat.diffuseColor = new Color3(1, 0, 0);
+
+			const ballInitMeshPosition = scene.getMeshByName("filet");
+			if (ballInitMeshPosition) {
+				this.resetPlacement = new Vector3(ballInitMeshPosition.getAbsolutePosition().x, -0.2, ballInitMeshPosition.getAbsolutePosition().z);
+			}
+
+			this.meshFinJ1 = new Mesh("finJ1", scene);
+			this.meshFinJ2 = new Mesh("finJ2", scene);
+
+			const finJ1 = scene.getMeshByName("finJ1");
+			const finJ2 = scene.getMeshByName("finJ2");
+			if(finJ1) {
+				this.meshFinJ1 = finJ1;
+			}
+			if(finJ2) {
+				this.meshFinJ2 = finJ2;
+			}
+
+			// add color to a ball
+			this._body.material = mat;
+
+			// set start speed and direction
+			this._speed = 0.01;
+			this._direction = 1;
+
+			// enable collision
+			this._body.checkCollisions = true;
+
+			this._paddle1 = paddle1;
+			this._paddle2 = paddle2;
+
+			// initialisation
+			this.resetBall = true;
+			// set start speed and direction
+			this._velocity = new Vector3(0.01, 0, 0.01);
+			this._body.position.copyFrom(this.resetPlacement);
+			this.setMinAndMax();
 	}
 
-	public update() {
-		if (!this.hasStarted) {
-			this.hasStarted = true;
-			this.startBallMovement();
-			return;
+	private isReadyToRun = false;
+
+	update() {
+		if (!this._isEndGame) {
+			if (this.indexGame - 1 >= this.maxGame) {
+				this._isEndGame = true;
+				console.log("End Game");
+				return;
+			}
+			if (this.resetBall && this.indexGame -1  <= this.maxGame) {
+				// attendre un peu avant de relancer la balle
+				this.resetBall = false;
+				this._body.position.copyFrom(this.resetPlacement);
+				this._velocity = new Vector3(0, 0, 0.04); // Reset velocity if needed
+				this.indexGame++;
+				this.isReadyToRun = false;
+				setTimeout(() => {
+					this.isReadyToRun = true;
+				}, 2000);
+				return;
+			}
+			if (this.isReadyToRun) {
+				this._move();
+				this._checkCollision();
+				this._checkBoundaryCollision();
+			}
 		}
-		this.ball.position.z += this.direction.z * this.speed;
-		console.log(this.ball.position.z, this.direction.z, this.speed);
 	}
 
-	public startBallMovement () {
-		let direction = Math.random() < 0.5 ? -1 : 1;
-		this.direction = new Vector3(0, 0, direction);
+	_move() {
+		let ballPos = this._body.position;
+
+        // Mise à jour de la position de la balle en utilisant le vecteur de vitesse
+        ballPos.x += this._velocity.x;
+        ballPos.z += this._velocity.z;
 	}
 
-	public initBall () : void { 
-		const ball = MeshBuilder.CreateSphere("ball", { diameter: 0.15 }, this.scene);
-		const getFiletMesh = this.scene.getMeshByName("filet");
-		if (getFiletMesh) {
-			ball.position = new Vector3(-getFiletMesh.position.x, getFiletMesh.position.y + 0.1, getFiletMesh.position.z);
+	private _checkCollision() {
+    const ballPos = this._body.position;
+    const paddle1Pos = this._paddle1._body.position;
+    const paddle2Pos = this._paddle2._body.position;
+
+    // Collision with Paddle 1
+		console.log(Math.abs(ballPos.x - paddle1Pos.x) < WIDTH / 2, Math.abs(ballPos.z - paddle1Pos.z) < HEIGHT / 2)
+    if (Math.abs(ballPos.x - paddle1Pos.x) < WIDTH / 2 && 
+			Math.abs(ballPos.z - paddle1Pos.z) < HEIGHT / 2) {
+			this.hitBallBack(this._paddle1);
+    }
+
+    // Collision with Paddle 2
+    if (Math.abs(ballPos.x - paddle2Pos.x) < WIDTH / 2 && 
+			Math.abs(ballPos.z - paddle2Pos.z) < HEIGHT / 2) {
+			this.hitBallBack(this._paddle2);
+    }
+	}
+
+	private hitBallBack(paddle: Paddle) {
+		this._velocity.x = (this._body.position.x - paddle._body.position.x) / 5;
+		this._velocity.z *= -1; // Inverse la direction z
+	}
+
+	private _checkBoundaryCollision() {
+    const ballPos = this._body.position;
+		// mur sur les côtés 
+    if (ballPos.x <= this.minX || ballPos.x >= this.maxX) {
+			this._velocity.x *= -1;
+    }
+    if (ballPos.z <= this.minZ ) {
+			this._score.player1++;
+			this.resetBall = true;
+    } else if (ballPos.z >= this.maxZ) { 
+			this.resetBall = true;
+			this._score.player2++;
 		}
-		// la mettre en rouge
-		const material = new StandardMaterial("texture1", this.scene);
-		material.emissiveColor = new Color3(1, 1, 0);
-		ball.material = material;
-		this.ball = ball;
+	}	
+
+	private setMinAndMax () {
+		const coteG = this._scene.getMeshByName("coteG");
+		if (coteG) {
+			coteG.isVisible = false;
+			this.maxX = coteG.getAbsolutePosition().x + -0.2;
+		}
+
+		const coteD = this._scene.getMeshByName("coteR");
+		if (coteD) {
+			coteD.isVisible = false;
+			this.minX = coteD.getAbsolutePosition().x + 0.2;
+		}
+		const finJ1 = this._scene.getMeshByName("finJ1");
+		if (finJ1) {
+			finJ1.isVisible = false;
+			this.minZ = finJ1.getAbsolutePosition().z;
+		}
+		const finJ2 = this._scene.getMeshByName("finJ2");
+		if (finJ2) {
+			finJ2.isVisible = false;
+			this.maxZ = finJ2.getAbsolutePosition().z;
+		}
 	}
+
+	public getScore() {
+		return this._score;
+	}
+};
+
+enum BallDirection {
+	UP = 1,
+	DOWN,
+	L_UP,
+	L_DOWN,
+	R_UP,
+	R_DOWN
+}
+enum PaddleDirection {
+	LEFT,
+	RIGHT,
+	NONE
+}
+
+const WIDTH = 0.4;
+const HEIGHT = 0.3;
+const DEPTH = 0.1;
+
+class Paddle {
+    _body: Mesh;
+    _type: string; //player or cpu
+    _direction: PaddleDirection;
+		_input : PlayerInputTennisGame;
+		speed = 0.01;
+
+    constructor(paddleType: string, scene: Scene, input: PlayerInputTennisGame) {
+			this._type = paddleType;
+			this._input = input;
+			this._body = MeshBuilder.CreateBox(this._type, {
+					depth: DEPTH,
+					width: WIDTH,
+					height: HEIGHT,
+			}, scene);
+
+			// prepare the material
+			let mat = new StandardMaterial('paddleMaterial', scene);
+			mat.emissiveColor = this._type == 'player' ? new Color3(0, 0, 1) : new Color3(1, 0, 0);
+
+			// add color to a paddle
+			this._body.material = mat;
+
+			// add a position
+			const player1Init = scene.getMeshByName("joueur1");
+			const player2Init = scene.getMeshByName("joueur2");
+			if (player1Init && player2Init) {
+				this._body.position = this._type == 'player' ? player1Init.getAbsolutePosition().clone() : player2Init.getAbsolutePosition().clone();
+			}
+
+			// enable collisions
+			this._body.checkCollisions = true;
+
+			// initial direction
+			this._direction = PaddleDirection.NONE;
+    }
+
+    handleEvent() {
+			if (this._type == 'player') {
+				this._body.position.x += this._input.horizontal * this.speed;
+				if (this._input.horizontal > 0) {
+					this._direction = PaddleDirection.RIGHT;
+				} else if (this._input.horizontal < 0) {
+					this._direction = PaddleDirection.LEFT;
+				} else {
+					this._direction = PaddleDirection.NONE;
+				}
+			}
+    }
+
+    moveByBallPosition(x:number) {
+			if(this._type=='cpu'){
+				if(x>this._body.position.x){
+					this._body.position.x+=0.3;
+					this._direction = PaddleDirection.LEFT;
+				}
+				if(x<this._body.position.x){
+					this._body.position.x-=0.3;
+					this._direction = PaddleDirection.RIGHT;
+				}
+				if(x==this._body.position.x){
+					this._direction = PaddleDirection.NONE;
+				}
+			}
+    }
+
+    getDirection(): PaddleDirection {
+			return this._direction;
+    }
 }
