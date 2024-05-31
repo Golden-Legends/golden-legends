1f84b561b97ad3e596ea6ecb01ee160e63cc9864
RemiSaurel <r.saurel5@gmail.com>
2024-05-31 17:19:46 +0200

    feat: loading screen

---
+++ src/components/gui/Loading.vue
@@ -0,0 +1,14 @@
+<script setup lang="ts"></script>
+
+<template>
+  <div class="flex flex-col items-center">
+    <span class="text-3xl font-bold mb-4"> Chargement en cours... </span>
+    <img
+      src="@/../public/timer_logo.svg"
+      alt="loading"
+      class="h-80 w-80 animate-spin"
+    />
+  </div>
+</template>
+
+<style scoped></style>
